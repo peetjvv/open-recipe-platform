3e856bc3e8dd72e68591826e74dd26de866b7908
Peet Janse van Vuuren <peetjvv@users.noreply.github.com>
2021-04-27 13:33:56 +0200

    Implemented Sentry and a basic error boundary (#9)

---
+++ react-app/src/app/components/error-boundary.tsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Primitive } from '@sentry/types';
+import { RouteDefinitions } from '../routes';
+
+const ErrorBoundaryFallback: React.FC<{
+  error: Error;
+  componentStack: string | null;
+  resetError: () => void;
+}> = ({ error, componentStack, resetError }) => (
+  <div className="bg-white dark:bg-grey-dark m-4">
+    <h1>Something went wrong</h1>
+    <p>{error.toString()}</p>
+    <p>{componentStack}</p>
+    <button onClick={() => resetError()}>Try to recover</button>
+    <Link to={RouteDefinitions.HOME.path}>Reload</Link>
+  </div>
+);
+
+const ErrorBoundary: React.FC<{
+  children: React.ReactNode;
+  showDialog?: boolean;
+  dialogOptions?: Sentry.ReportDialogOptions;
+  tags?: { [key: string]: Primitive };
+}> = props => {
+  const { children, showDialog = true, dialogOptions = {}, tags = {} } = props;
+
+  return (
+    <Sentry.ErrorBoundary
+      fallback={({ error, componentStack, resetError }) => (
+        <ErrorBoundaryFallback
+          error={error}
+          componentStack={componentStack}
+          resetError={resetError}
+        />
+      )}
+      showDialog={showDialog}
+      dialogOptions={{
+        title: 'Something went wrong',
+        ...dialogOptions,
+        user: {
+          name: undefined, // TODO: pull name from auth
+          email: undefined, // TODO: pull email from auth
+          ...(dialogOptions.user || {}),
+        },
+      }}
+      beforeCapture={scope => {
+        if (!!tags) {
+          scope.setTags(tags);
+        }
+        // TODO: add auth details to scope
+        // TODO: add tag for recipeId if any
+      }}
+    >
+      {children}
+    </Sentry.ErrorBoundary>
+  );
+};
+
+export default ErrorBoundary;
--- react-app/src/app/index.tsx
+++ react-app/src/app/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { useReducer } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import TopBar from './top-bar';
 import Home from './home';
-import { Anchor, Error } from './components';
+import { Anchor, Error, ErrorBoundary } from './components';
 import { AllActions, combinedReducer, initialState, State } from '../data';
 import Recipes from './recipes';
 import { RouteDefinitions } from './routes';
@@ -20,50 +20,52 @@ const App: React.FC<{}> = props => {
 
   return (
     <Router>
-      <div className="flex flex-col items-center h-screen font-body text-black dark:text-grey-light">
-        <div className="page-container flex-grow flex flex-col my-16">
-          <TopBar themeSuite={state.theme.suite} dispatch={dispatch} />
-          <div className="content-panel flex-grow flex rounded-lg bg-white dark:bg-grey-dark bg-opacity-90 dark:bg-opacity-10">
-            <Switch>
-              <Route exact path={RouteDefinitions.HOME.path}>
-                <Home state={state} dispatch={dispatch} />
-              </Route>
-              <Route path={RouteDefinitions.RECIPES.path}>
-                <Recipes state={state} dispatch={dispatch} />
-              </Route>
-              {/* Keep this very last since it's the fallback in case no route matched */}
-              <Route path="*">
-                <Error errorCode={404} />
-              </Route>
-            </Switch>
+      <ErrorBoundary>
+        <div className="flex flex-col items-center h-screen font-body text-black dark:text-grey-light">
+          <div className="page-container flex-grow flex flex-col my-16">
+            <TopBar themeSuite={state.theme.suite} dispatch={dispatch} />
+            <div className="content-panel flex-grow flex rounded-lg bg-white dark:bg-grey-dark bg-opacity-90 dark:bg-opacity-10">
+              <Switch>
+                <Route exact path={RouteDefinitions.HOME.path}>
+                  <Home state={state} dispatch={dispatch} />
+                </Route>
+                <Route path={RouteDefinitions.RECIPES.path}>
+                  <Recipes state={state} dispatch={dispatch} />
+                </Route>
+                {/* Keep this very last since it's the fallback in case no route matched */}
+                <Route path="*">
+                  <Error errorCode={404} />
+                </Route>
+              </Switch>
+            </div>
+          </div>
+
+          <div className="absolute z-0 bottom-0 right-0 px-2 py-1 bg-white dark:bg-grey-dark bg-opacity-90 dark:bg-opacity-10 text-sm">
+            <p>
+              Background photo by{' '}
+              {state.theme.suite === 'DARK' ? (
+                <Anchor
+                  target="_blank"
+                  href="https://unsplash.com/@nordwood?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >
+                  NordWood Themes
+                </Anchor>
+              ) : (
+                <Anchor
+                  target="_blank"
+                  href="https://unsplash.com/@brookelark?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+                >
+                  Brooke Lark
+                </Anchor>
+              )}{' '}
+              on{' '}
+              <Anchor href="https://unsplash.com/s/photos/cooking?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+                Unsplash
+              </Anchor>
+            </p>
           </div>
         </div>
-
-        <div className="absolute z-0 bottom-0 right-0 px-2 py-1 bg-white dark:bg-grey-dark bg-opacity-90 dark:bg-opacity-10 text-sm">
-          <p>
-            Background photo by{' '}
-            {state.theme.suite === 'DARK' ? (
-              <Anchor
-                target="_blank"
-                href="https://unsplash.com/@nordwood?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-              >
-                NordWood Themes
-              </Anchor>
-            ) : (
-              <Anchor
-                target="_blank"
-                href="https://unsplash.com/@brookelark?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-              >
-                Brooke Lark
-              </Anchor>
-            )}{' '}
-            on{' '}
-            <Anchor href="https://unsplash.com/s/photos/cooking?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-              Unsplash
-            </Anchor>
-          </p>
-        </div>
-      </div>
+      </ErrorBoundary>
     </Router>
   );
 };
